5ac4c9cdb688eb59290a46badf3ed183e1b1684f
BirdSwrgy <birdaoswargiary28@gmail.com>
2025-11-09 13:17:52 +0530

    login page

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,6 @@
-import { Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
+import { Link } from "expo-router";
+//import { Button } from "@react-navigation/elements";
 
 export default function Index() {
   return (
@@ -10,6 +12,14 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+    <Text style={{ fontSize: 22, marginBottom: 20 }}>Welcome to Attendance App</Text>
+
+      {/* Go to the login page */}
+      <Link href="/(loginUI)/login" asChild>
+      <Button title="Go to Login " />
+      </Link>
     </View>
+
+    
   );
 }
